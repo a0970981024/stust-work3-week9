d923dfdb9bbea1386dcd472ef5345a81c5885980
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-04-12 14:26:17 +0800

    計算一串英文字母被使用的頻率 以小寫a為開頭往後排序加入專案檔案。

---
+++ vs-work3-week9.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std ;
+int main(){
+	char sentence[100]={0}, cnumber[100] = { 0 };
+	int len, z = 0, head = 1, inumber[100] = { 0 },temp=0, cpnumber[100] = { 0 };
+	cin.getline(sentence, 100);
+	
+	len = strlen(sentence);
+
+	for (int i = 0; i < len; i++) {
+		if (sentence[i] == ' ') head++;
+		int x = 1, y = 0;
+		for (int t = 0; t < i; t++) {
+			if (sentence[i] == sentence[t] || sentence[i] == (sentence[t] - 32) || sentence[i] == (sentence[t] + 32)) y += 1;
+		}
+
+		if (y == 0 && sentence[i] != ' ') {
+			if(sentence[i]<97){
+				sentence[i]+=  32;
+				cpnumber[temp] = sentence[i];
+			}else cpnumber[temp] = sentence[i];
+
+			for (int j = 0; j < len; j++) {
+				if (sentence[i] == sentence[j] || sentence[i] == (sentence[j] - 32) || sentence[i] == (sentence[j] + 32)) inumber[temp] += 1;
+			}
+			temp++;
+			z++;
+		}
+		
+	}
+
+	for (int i = 0; i < z; i++) {
+		for (int j = 0; j < z-1; j++) {
+			if (cpnumber[j] > cpnumber[j + 1]) {
+				cpnumber[j] += cpnumber[j + 1];//a=a+b
+				cpnumber[j + 1] = cpnumber[j] - cpnumber[j + 1];//b=a-b
+				cpnumber[j] -= cpnumber[j + 1];//a=a-b
+
+				inumber[j] += inumber[j + 1];
+				inumber[j + 1] = inumber[j] - inumber[j + 1];
+				inumber[j] -= inumber[j + 1];
+			}
+		}
+	}
+	for (int i = 0; i < z; i++) {
+		cnumber[i] = cpnumber[i];
+	}
+
+	for (int i = 0; i < z; i++) {
+		cout << cnumber[i] << ":" << inumber[i]<<endl;
+	}
+
+}
