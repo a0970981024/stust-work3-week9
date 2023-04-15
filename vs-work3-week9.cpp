89efedd09ac5f2486f5b881de08f2341e98ebd48
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-04-15 17:19:44 +0800

    空格及換行

--- vs-work3-week9.cpp
+++ vs-work3-week9.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main() {
 	string sentence, word;
 	int appear = 0;
-
-	do {
+	cin >> word >> sentence;
+	/*do {
 		getline(cin, word);
 	} while (size(word) > 128);
 
 	do {
 		getline(cin, sentence);
-	} while (size(sentence) > 512);
+	} while (size(sentence) > 512);*/
 	
 	for (int i = 0; i < size(sentence)- (size(word)-1); i++) {
 		int temp = 0;
@@ -29,9 +29,9 @@ int main() {
 		if(temp==size(word)) appear++;
 	}
 
-	if (appear != 0) {
+	
 		cout << appear;
-	}
+	
 	
 
 }
