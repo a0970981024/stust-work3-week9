e9bde2827cbdef6922506b7c1724e18f0a979d84
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-05-19 12:49:25 +0800

    新增註解

--- vs-work3-week9.cpp
+++ vs-work3-week9.cpp
@@ -7,11 +7,13 @@ using namespace std;
 
 int main() {
 	string sentence;
+	//原本32~125位元	
 	string Demo = "abcdefghijklmnopqrstuvwxyz1234567890-=[]\\;',./ ";
+	//右移一位後32~125位元
 	string change = "snvfrghjokl;,mp[wtdyibecux234567890-==]\\\\''.// ";
 	int r = 0;
 	
-	//限制為 ASCII 表中，編號 32 至 125 之字元 
+	//只可輸入32~125位元(ASSIC)
 	do {
 		getline(cin, sentence);
 		r = 0;
@@ -19,13 +21,15 @@ int main() {
 			if (sentence[i] < 32 || sentence[i]>125) {
 				r++;
 			}
+			//打字串英文轉為小寫
 			if (sentence[i] >= 65 && sentence[i] <= 90) {
 				sentence[i] += 32;
 			}
 		}
-		
 	} while (r != 0);
 
+	//用輸入字串與原本的32~125位元比較
+	//找到相同為數時 列印右移後的32~125位元
 	for (int i = 0; i < size(sentence); i++) {
 		for (int j = 0; j < size(Demo); j++) {
 			if (sentence[i] == Demo[j]) {
