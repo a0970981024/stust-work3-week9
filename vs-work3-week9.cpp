579aef0a0a30db7f5b9c47a79d8217d1596653b5
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-05-19 14:44:25 +0800

    一維轉二維

--- vs-work3-week9.cpp
+++ vs-work3-week9.cpp
@@ -5,21 +5,35 @@ using namespace std;
 
 
 int main(){
-	int n, sd[30][30];
+	int n=0, temp[60]={0},sd[30][2]={0};
 	cout << "輸入n:";
 	do {
 		cin >> n;
 	} while (n < 1 || n > 30);
 	cout << "輸入sd :";
-
 	for (int i = 0; i < n * 2; i++) {
 		do {
-			cin >> sd[i];
-		} while (sd[i] < 1 || sd[i]>24);
+			cin >> temp[i];
+		} while (temp[i] < 1 || temp[i]>24);
 	}
+
+
+	int j = 0;
+	for (int i = 0; i < n * 2; i++) {
+		
+		if (i!= 0) {
+			if (i % 2 != 0) {
+				sd[j][1] = temp[i];
+				j++;
+			}else {
+				sd[j][0] = temp[i];
+			}
+		}else {
+			sd[0][0] = temp[i] ;
+		}
+	}
+
 	
-
-
 
 
    
